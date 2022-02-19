b1004def04ad19dc7fa8ce3ade57d5358470652e
Paulo-Sergio-Oliveira <99352391+Paulo-Sergio-Oliveira@users.noreply.github.com>
2022-02-19 18:44:06 -0300

    Commit
    Commit inicial

---
+++ script.js
@@ -0,0 +1,15 @@
+function pegarDados(){
+    document.querySelector(".esconder").style.display = "block";
+    let camText = document.getElementById("texto").value;
+    document.getElementById("camText").innerHTML = camText;
+    // if(document.getElementById("info2").checked) {
+    // alert("Seu Curso é Informática");
+    // }else if (document.getElementeById("adm2").checked){
+    // alert("Seu Curso é Administração");
+    // }else if (document.getElementeById("enf2").checked) {
+    // alert("Seu Curso é Enfermargem");
+    // }
+    // let priFaixa = document.getElementById("PriFaixa").value;
+    // alert(priFaixa);
+    // }
+    }
